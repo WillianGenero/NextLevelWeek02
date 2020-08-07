2e82d01e137ffebfbf2e018effb0dbcacc44d9cd
Willian Genero <willian-b-g@hotmail.com>
2020-08-07 15:56:58 -0300

    Configuração de navegação SPA com react-router-dom

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Landing from './pages/Landing'
+import React from 'react'
+import Routes from './routes'
 
 import './assets/styles/global.css'
 
 function App() {
   return (
-    <Landing />
-  );
+    <Routes />
+  )
 }
 
-export default App;
+export default App
