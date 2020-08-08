dcc1247d05f81205a6aba911f4dba4d602d686a8
Willian Genero <willian-b-g@hotmail.com>
2020-08-07 21:46:55 -0300

    Criação dos itens de professores em componente

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './styles.css'
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/31111020?s=460&u=a696c1cebcfe3154f6f7db96804c73a0580d2fb7&v=4" alt="Willian Bordignon Genero"/>
+        <div>
+          <strong>Willian Bordignon Genero</strong>
+          <span>Química</span>
+        </div>
+      </header>
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /><br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vidas das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 50,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp"/>
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  )
+}
+
+export default TeacherItem
